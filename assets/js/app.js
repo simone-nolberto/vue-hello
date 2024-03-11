d6b8a4af0a41ec9ecef000c6edc88d6f8217da0a
simone-nolberto <simone.nolberto@outlook.com>
2024-03-11 11:58:09 +0100

    add title print

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,14 @@
 console.log('it works!');
+
+const {createApp} = Vue 
+
+    createApp({
+        data(){
+            return{
+                message: 'Vue Hello',
+                image: 'dragonball.jpeg'
+            }
+        }
+    }).mount('#app');
+        
+    
